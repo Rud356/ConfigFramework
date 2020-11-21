445fbb19f55f462209afc218f4ef8e29549a5f18
Rud356 <r.moroshkin2015@gmail.com>
2020-11-21 20:03:53 +0300

    Added fix to setup.py and updated .gitignore

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
