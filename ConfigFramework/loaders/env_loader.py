81f336d823523289f431e26dfed04c29e2a2b74d
Rud356 <r.moroshkin2015@gmail.com>
2021-03-06 21:25:27 +0300

    Add more loaders

---
+++ ConfigFramework/loaders/env_loader.py
@@ -0,0 +1,20 @@
+from os import environ
+from typing import Dict, NoReturn
+
+from ConfigFramework.abstract.abc_loader import AbstractConfigLoader
+
+
+class EnvLoader(AbstractConfigLoader):
+    @classmethod
+    def load(cls, defaults: Dict = None):
+        return cls(data=environ, defaults=defaults)
+
+    def dump(self, include_defaults: bool = False) -> NoReturn:
+        """
+        You can not dump values to environ yet because I don't know a good way to do that or think it's needed.
+        Left only for compatibility purpose.
+
+        :param include_defaults: doesn't affect anything as stated before.
+        :return:
+        """
+        pass
