f32e9b6bef012607959d16ed0439eda07b8dffc8
Rud356 <r.moroshkin2015@gmail.com>
2021-03-06 20:47:05 +0300

    Add abstract loader and variable classes Add CompositeLoader class

---
+++ ConfigFramework/abstract/abc_loader.py
@@ -0,0 +1,162 @@
+from abc import ABC, abstractmethod
+from collections import ChainMap, Mapping
+from functools import lru_cache
+from pathlib import Path
+from typing import Any, AnyStr, Dict, Hashable, NoReturn, Optional, Tuple, Union, final
+
+
+class AbstractConfigLoader(ABC, Mapping):
+    """
+    Base class for any of your loaders. Initialization can be mae through load function, or, if author wants so,
+    through `__init__` function.
+    """
+
+    def __init__(self, data: Union[Dict, ChainMap, Mapping], defaults: Dict, *args, **kwargs):
+        """
+        Creates new config loader with your variables.
+
+        :param data: data from loader.
+        :param defaults: default variables that will be used if not found in loader.
+        :param args: arguments that can be used for your custom loaders.
+        :param kwargs: keyword arguments that can be used for your custom loaders.
+        """
+        self.data = data
+        self.defaults = defaults
+
+        if isinstance(defaults, dict):
+            self.lookup_data = ChainMap(self.data, self.defaults)
+
+        else:
+            self.lookup_data = self.data
+
+    @classmethod
+    @abstractmethod
+    def load(cls, *args, **kwargs):
+        """
+        Initializes creation of new ConfigLoader.
+        You must provide your arguments that are needed for your loader.
+
+        :param args: arguments that can be used for your custom loaders.
+        :param kwargs: keyword arguments that can be used for your custom loaders.
+        """
+        pass
+
+    @abstractmethod
+    def dump(self, include_defaults: bool = False) -> NoReturn:
+        """
+        Dumps updated variables.
+
+        :param include_defaults: specifies if you want to have default variables to be.
+        :return:
+        """
+        pass
+
+    def dump_to(self, other_loader: 'AbstractConfigLoader', include_defaults: bool = False) -> NoReturn:
+        """
+        Dumps variables to other loader that been already initialized.
+
+        :param other_loader: other loader that already initialized and where you want to dump stuff.
+        :param include_defaults: include_defaults: specifies if you want to have default variables to be.
+        :return:
+        """
+        if isinstance(other_loader, AbstractConfigLoader):
+            if include_defaults:
+                other_loader.lookup_data = self.lookup_data
+
+            else:
+                other_loader.lookup_data = self.data
+
+        else:
+            raise ValueError(
+                f"{self} got invalid loader to dump variables to. Got type: {type(other_loader)}"
+            )
+
+        other_loader.dump(include_defaults)
+
+    def get(self, key: Union[Hashable, AnyStr], default=None) -> Any:
+        """
+        Returns an item under specified key or key as path and if
+        it didn't find it - returns default variable.
+
+        :param key: a key that points at where you want to grab variable.
+        :param default: default value.
+        :return: anything lying there.
+        """
+        casted_key = self._key_to_path_cast(key)
+        val_root = self._get_to_variable_root(casted_key)
+
+        return val_root.get(casted_key[-1], default=default)
+
+    @staticmethod
+    @final
+    @lru_cache(maxsize=None)
+    def _key_to_path_cast(key: Union[AnyStr, Hashable]) -> Union[Tuple[AnyStr, ...], Tuple[Hashable, ...]]:
+        """
+        Casts a key to tuple of keys, that should be applied one by one to get to where variable lies.
+        Path pointing example: config_root/database/database_ip
+
+        :param key: string with path, pointing at out variable or hashable.
+        :return: tuple of sub keys.
+        """
+        if isinstance(key, str) and ("/" in key):
+            return Path(key).parts
+
+        return key,
+
+    def _get_to_variable_root(
+            self, keys: Union[Tuple[AnyStr, ...], Tuple[Hashable, ...]], lookup_at: Optional[Dict] = None
+    ) -> Dict:
+        """
+        Returns an dictionary with our variable.
+
+        :param keys: keys tuple we apply to get to root of variable.
+        :param lookup_at: the location we're looking at. In case we need to lookup at specific part of our loader.
+        :return:
+        """
+        root = lookup_at or self.lookup_data
+        # Last part of key must be the variable
+        for key in keys[:-1]:
+            root = root[key]
+
+        return root
+
+    def __getitem__(self, key: Union[Hashable, AnyStr]) -> Any:
+        """
+        Returns an item under specified key or key as path and if
+        it didn't find it - raises KeyError.
+
+        :param key:
+        :return:
+        """
+        casted_key = self._key_to_path_cast(key)
+        val_root = self._get_to_variable_root(casted_key)
+
+        return val_root[casted_key[-1]]
+
+    def __setitem__(self, key: Union[Hashable, AnyStr], value: Any) -> NoReturn:
+        """
+        Sets variable value inside of your loader.
+
+        :param key: a key that points at what variable you want to set value to.
+        :param value: a new value for variable.
+        :return:
+        """
+
+        casted_key = self._key_to_path_cast(key)
+        val_root = self._get_to_variable_root(casted_key)
+        val_root[casted_key[-1]] = value
+
+    def __repr__(self):
+        return f"Loader: {self.__class__.__name__}"
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def __len__(self) -> int:
+        return len(self.lookup_data)
+
+    def __iter__(self):
+        return iter(self.lookup_data)
+
+
+__all__ = ["AbstractConfigLoader"]
---
+++ ConfigFramework/abstract/abc_variable.py
@@ -0,0 +1,132 @@
+from abc import ABC
+from functools import wraps
+from typing import Any, AnyStr, Callable, Hashable, NoReturn, Optional, TYPE_CHECKING, Type
+
+if TYPE_CHECKING:
+    from .abc_loader import AbstractConfigLoader
+
+
+class AbstractConfigVar(ABC):
+    """
+    Abstract config variable with descriptors interface which is a base type for any of your variables classes.
+
+    """
+    def __init__(
+        self, key: [Hashable, AnyStr], loader: AbstractConfigLoader, *,
+        typehint: Optional[Type] = Any,
+        caster: Optional[Callable] = None, dump_caster: Optional[Callable] = None,
+        validator: Optional[Callable] = None, default: Optional[Any] = None
+    ):
+        """
+        Initializes variable for specified loader and key.
+
+        :param key: Any hashable or a string. Strings can be written as paths in case you need a variable.
+         that underlies other mappings. Example of how to get such vars: `config_root/database/database_ip`.
+        :param loader: A loader that will be looked up to get vars value or to update values.
+        :param typehint: Typehint for _value field, that by default being returned.
+        :param caster: Callable that should return variable casted to specific type (in case you need custom types).
+        :param dump_caster: Callable that being called when config being dumped.
+        :param validator: Callable that validates value or defaults in case the original value is invalid.
+        :param default: Default value that will be set, if value is invalid.
+        """
+        self.key = key
+        self.loader: AbstractConfigLoader = loader
+
+        # Redefining functions that we will need if they are provided
+        for redefine_func_name, func in zip(
+            ("caster", "dump_caster", "validate"),
+            (caster, dump_caster, validator)
+        ):
+            if callable(func):
+                self.__dict__[redefine_func_name] = func
+
+        if default is not None:
+            self._value: typehint = self.caster(loader.get(key, default))
+        else:
+            self._value: typehint = self.caster(loader[key])
+
+        # Validation of value and defaults
+        bool_casted_validator: Callable = self._validate_to_bool(self.validate)
+        default = caster(default)
+
+        if not bool_casted_validator(self._value):
+            if (default is not None) and self._validate_to_bool(self.validate(default)):
+                self._value: typehint = default
+
+            elif (
+                (getattr(loader, "defaults", None) is not None) and
+                bool_casted_validator(
+                    caster(loader._get_to_variable_root(
+                        loader._key_to_path_cast(key),
+                        lookup_at=loader.defaults
+                    ))
+                )
+            ):
+                self._value: typehint = caster(loader._get_to_variable_root(
+                    loader._key_to_path_cast(key), lookup_at=loader.defaults
+                ))
+
+            else:
+                raise ValueError(f"Invalid value for {self} in {self.loader} and default values not found")
+
+    def caster(self, value: Any) -> Any:
+        """
+        Callable that should return variable casted to specific type (in case you need custom types).
+
+        :param value: value to be casted
+        :return:
+        """
+        return value
+
+    def dump_caster(self, value: Any) -> Any:
+        """
+        Callable that being called when config being dumped.
+        :param value: value to be casted before being dumped
+        :return:
+        """
+        return value
+
+    def validate(self, value: Any) -> bool:
+        """
+        Callable that validates value or defaults in case the original value is invalid.
+        :param value: value to be validated
+        :return:
+        """
+        return True
+
+    @staticmethod
+    def _validate_to_bool(func):
+        """
+        Wrapper that returns a bool value representing if check failed or raised ValueError.
+
+        :param func:
+        :return:
+        """
+        @wraps(func)
+        def execute(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+
+            except ValueError:
+                return False
+
+        return execute
+
+    def __get__(self, instance, owner=None) -> Any:
+        return self._value
+
+    def __set__(self, instance, value: Any) -> NoReturn:
+        if not self.validate(value):
+            raise ValueError(f"Invalid value to be set for property {self}")
+
+        self._value = value
+        self.loader[self.key] = value
+
+    def __str__(self):
+        return self.key
+
+    def __repr__(self):
+        return f"{self} in {self.loader} = {self._value}"
+
+
+__all__ = ["AbstractConfigVar"]
